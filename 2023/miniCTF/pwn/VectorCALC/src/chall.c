0abbea85c9842f0c0f2a8e7a61760c3579f6a15f
w1n_gl0ry <duynguyen45608@gmail.com>
2023-07-29 23:36:27 +0700

    Update chall.c
    ok
    
    Signed-off-by: w1n_gl0ry <duynguyen45608@gmail.com>

--- 2023/miniCTF/pwn/VectorCALC/src/chall.c
+++ 2023/miniCTF/pwn/VectorCALC/src/chall.c
@@ -1,35 +1,185 @@
-0x577020 
-,0x302071 
-,0x797022 
-,0x612073 
-,0x357024 
-,0x712075 
-,0x597026 
-,0x372077 
-,0x787028 
-,0x3a2079 
-,0x78702a 
-,0x6a207b 
-,0x78702c 
-,0x3c207d 
-,0x3e702e 
-,0x61207f
-,0x4f7030
-,0x672061
-,0x737032
-0x662063
-0x787034
-0x612065
-0x497036
-0x702067
-0x777038
-0x762069
-0x7e703a
-0x44206b
-0x76703c
-0x2d206d
-0x7c703e
-0x62206f 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-enc=[]
+#define MAX_FAVES 4
+#define MAX_VECTORS 3
+struct Vector{
+	__uint64_t x;
+    __uint64_t y;
+    void (*printFunc)(struct Vector*);
+};
 
+struct Vector v_list[MAX_VECTORS];
+__uint64_t* sum;
+void* faves[MAX_FAVES];
+
+void printData(struct Vector* v);
+
+void enterData(){
+    struct Vector* v;
+    __uint64_t idx;
+
+    printf("Index: ");
+    scanf("%lu",&idx);
+    
+    if(idx > MAX_VECTORS){
+        puts("Invaild index!");
+        exit(-1);
+    }
+
+    v = &v_list[idx];
+
+    v->printFunc = printData;
+    printf("Enter x: ");
+    scanf("%lu",&v->x);
+    printf("Enter y: ");
+    scanf("%lu",&v->y);
+}
+
+
+void printData(struct Vector* v){
+    puts("Data: ");
+    printf("v = [%lu %lu]\n",v->x,v->y);
+}
+
+void sumVector(){
+    __uint64_t idx;
+    printf("Save the sum to index: ");
+    scanf("%lu",&idx);
+    
+    if(idx > MAX_VECTORS){
+        puts("Invaild index!");
+        exit(-1);
+    }
+
+    sum = &v_list[idx];
+    for(__uint64_t i = 0 ; i < MAX_VECTORS ;++i){
+        if( i != idx){
+            ((struct Vector *)sum)->x += v_list[idx].x;
+            ((struct Vector *)sum)->y += v_list[idx].y;
+        }
+    }
+}
+
+void loadFavorite(){
+    if(sum == NULL){
+        puts("You must set the sum before!");
+        return;
+    }
+    __uint64_t idx;
+
+    printf("Index: ");
+    scanf("%lu",&idx);
+    
+    if(idx >= MAX_FAVES){
+        puts("Invaild index!");
+        exit(-1);
+    }
+
+    faves[idx] = malloc(sizeof(struct Vector));
+
+    ((struct Vector *)faves[idx])->printFunc = printData;
+
+    memcpy(faves[idx],&sum[idx], sizeof(struct Vector));
+}
+
+void printFavorite(){
+    if(sum == NULL){
+        puts("You must set the sum before!");
+        return;
+    }
+
+    __uint64_t idx;
+
+    printf("Index: ");
+    scanf("%lu",&idx);
+    
+    if(idx >= MAX_FAVES || faves[idx] == NULL){
+        puts("Invaild index!");
+        exit(-1);
+    }
+    if( ((__uint64_t *)faves[idx])[2] )
+        ((struct Vector *)faves[idx])->printFunc(faves[idx]);
+    else 
+        ((struct Vector *)sum)->printFunc(faves[idx]);
+}
+
+void addFavorute(){
+
+    __uint64_t idx;
+
+    printf("Index: ");
+    scanf("%lu",&idx);
+    
+    if(idx >= MAX_FAVES || faves[idx] == NULL){
+        puts("Invaild index!");
+        exit(-1);
+    }
+
+    ((struct Vector *)sum)->x += ((struct Vector *)faves[idx])->x;
+    ((struct Vector *)sum)->y += ((struct Vector *)faves[idx])->y;
+}
+
+void init(){
+    setbuf(stdin,NULL);
+    setbuf(stdout,NULL);
+    for(__uint64_t i = 0 ; i < MAX_VECTORS ;++i){
+        v_list[i].printFunc = printData;
+    }
+}
+
+void printMenu(){
+    printf(
+        "\r\n"
+        "1. Enter data.\n"
+        "2. Sum vector.\n"
+        "3. Print sum vector\n"
+        "4. Save sum to favorite\n"
+        "5. Print favorite\n"
+        "6. Add favorite to the sum\n"
+        "> "
+    );
+}
+
+int main(int argc, char** argv, char** envp){
+    init();
+    __uint32_t choice ;
+    while(1){
+        printMenu();
+        scanf("%u", &choice);
+        switch (choice)
+        {
+        case 1:
+            enterData();
+            break;
+        
+        case 2:
+            sumVector();
+            break;
+        
+        case 3:
+            ((struct Vector *)sum)->printFunc(sum);
+            break;
+
+        case 4:
+            loadFavorite();
+            break;
+        
+        case 5:
+            printFavorite();
+            break;
+        
+        case 6:
+            addFavorute();
+            break;
+
+        default:
+            puts("Invaild option!");
+            exit(-1);
+        }
+    }
+}
+
+void w1n(); // try to view the code in a disassembler :)
